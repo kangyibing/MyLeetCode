ebafb66eccb8c7dc72c8a33b59efb1bd601cf5dd
kangyibing <kangyibing@baidu.com>
2019-01-09 19:11:42 +0800

    first

---
+++ easy/771.py
@@ -0,0 +1,11 @@
+class Solution:
+    def numJewelsInStones(self, J, S):
+        """
+        :type J: str 宝石
+        :type S: str 拥有的石头
+        :rtype: Return type int 
+        """
+        n=0
+        for i in J: 
+            n+=S.count(i) #该方法返回i在S中出现的次数
+        return n
