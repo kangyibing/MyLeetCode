21d474f1e7d066cb1022927aafcb193f3f6e839a
kangyibing <kangyibing@baidu.com>
2019-01-09 19:08:18 +0800

    first

---
+++ 709.py
@@ -0,0 +1,17 @@
+class Solution:
+    def toLowerCase(self, str):
+        """
+        :type str: str
+        :rtype: str
+        """
+        #解法一
+        # str=str.lower()
+        # return str
+        #解法二
+        n=len(str)
+        temp=list(str) # list()方法用于将元组或字符串转换为列表
+        for i in range(n):
+            if temp[i]>='A' and temp[i]<='Z':
+                x= ord(temp[i])+32 # ord()返回对应字符的 ASCII 数值
+                temp[i]=chr(x) # chr()返回当前整数对应的ascii字符
+        return ''.join(temp) #join()返回通过指定字符连接序列中元素后生成的新字符串
---
+++ 929.py
@@ -0,0 +1,22 @@
+class Solution:
+    def numUniqueEmails(self, emails):
+        """
+        :type emails: List[str]
+        :rtype: int
+        """
+        raw_emails = set() #创建一个集合
+        for email in emails:
+            sp_at = email.split('@')
+            pre = sp_at[0].split('+')
+            raw_emails.add(pre[0].replace('.', '') + sp_at[1])
+
+        return len(raw_emails)
+        
+
+if __name__ == "__main__":
+    emails = [
+                "test.email+alex@leetcode.com",
+                "test.e.mail+bob.cathy@leetcode.com",
+                "testemail+david@lee.tcode.com"
+             ]
+    print(Solution().numUniqueEmails(emails))
